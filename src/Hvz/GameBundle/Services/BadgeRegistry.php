715bc3515ce940f2b6213321d3e11d10fe23eb6c
Sam Bloomberg <sam@redxdev.com>
2014-07-20 15:32:39 -0400

    added initial badge handling code

---
+++ src/Hvz/GameBundle/Services/BadgeRegistry.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Hvz\GameBundle\Services;
+
+class BadgeRegistry
+{
+	private $entityManager;
+
+	private $manualRegistry = array();
+
+	private $autoRegistry = array();
+
+	public function __construct($entityManager)
+	{
+		$this->entityManager = $entityManager;
+
+		$this->registerBadges();
+	}
+
+	public function handleInfection($profile)
+	{
+		$badges = $profile->getBadges();
+
+		foreach($autoRegistry as $badge)
+		{
+			if($badge['function']($profile))
+			{
+				$badges[$badge['id']] = true;
+			}
+		}
+
+		$profile->setBadges($badges);
+		$entityManager->flush();
+	}
+
+	/**
+	 * @param id badge id
+	 * @param name human-readable name
+	 * @param description human-readable description
+	 * @param imgPath path relative to public/images/badges
+	 * @param auto if true this badge will automatically be applied to players
+	 * @param function the auto function
+	 */
+	public function registerBadge($id, $name, $description, $imgPath, $auto = false, $function = null)
+	{
+		if($auto)
+		{
+			$this->autoRegistry[$id] = array(
+				'name' => $name,
+				'image' => $imgPath,
+				'function' => $function
+			);
+		}
+		else
+		{
+			$this->manualRegistry[$id] = array(
+				'name' => $name,
+				'image' => $imgPath,
+			);
+		}
+	}
+
+	public function registerBadges()
+	{
+		// double kill badge
+		registerBadge(
+			'kill-2x',
+			'Double Kill',
+			'Killed two humans within one hour',
+			'test1.gif',
+			true,
+			function($profile) {
+				$qb = $qb = $this->getEntityManager()->createQueryBuilder();
+				$query = $qb->select('count(s.id)')
+							->from('HvzGameBundle:InfectionSpread', 's')
+							->where('s.zombie = :zombie')
+							->setParameter('zombie', $profile)
+							->getQuery();
+
+				if($query->getSingleScalarResult() >= 2)
+				{
+					return true;
+				}
+
+				return false;
+			}
+		);
+	}
+}
