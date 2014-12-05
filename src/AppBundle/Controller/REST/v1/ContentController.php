6eb39f7a3d0f24ebf4a0f50c323f733efad77aa9
Sam Bloomberg <sam@redxdev.com>
2014-12-05 00:56:00 -0500

    REST api should be finished

--- src/AppBundle/Controller/REST/v1/ContentController.php
+++ src/AppBundle/Controller/REST/v1/ContentController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller\REST\v1;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContentController extends Controller
@@ -18,5 +19,28 @@ class ContentController extends Controller
         $contentManager = $this->get('content_manager');
 
         return new JsonResponse($contentManager->getRulesetList());
+    }
+
+    /**
+     * @Route("/missions", name="rest_v1_missions")
+     * @Method({"GET"})
+     */
+    public function missionsAction(Request $request)
+    {
+        $gameAuth = $this->get('game_authentication');
+
+        $apikey = $request->query->get('apikey');
+        $result = $gameAuth->processApiKey($apikey);
+
+        if($result["status"] !== "ok")
+        {
+            return new JsonResponse($result, 400);
+        }
+
+        $user = $result["user"];
+
+        $contentManager = $this->get('content_manager');
+
+        return new JsonResponse($contentManager->getTeamMissionList($user->getTeam()));
     }
 }
