347b1c73449f7a357e581deb3b471b72ddb2c1a2
Sam Bloomberg <sam@redxdev.com>
2015-04-07 19:19:56 -0400

    update.sh formatting

--- update.sh
+++ update.sh
@@ -2,11 +2,21 @@
 echo "Pulling from source..."
 git pull origin
 
+echo ""
+
 echo "Installing composer dependencies..."
 php composer.phar install
+
+echo ""
 
 echo "Dumping autoload..."
 php composer.phar dump-autoload --optimize
 
+echo ""
+
 echo "Dumping assets..."
 php app/console assetic:dump --no-debug --env=prod
+
+echo ""
+
+echo "Done!"
