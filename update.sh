c671b7159904e8a6a3d56d878d9488399618817b
Sam Bloomberg <sam@redxdev.com>
2015-04-07 19:18:56 -0400

    updated update.sh

--- update.sh
+++ update.sh
@@ -2,8 +2,11 @@
 echo "Pulling from source..."
 git pull origin
 
+echo "Installing composer dependencies..."
+php composer.phar install
+
+echo "Dumping autoload..."
+php composer.phar dump-autoload --optimize
+
 echo "Dumping assets..."
 php app/console assetic:dump --no-debug --env=prod
-
-echo "Clearing cache..."
-php app/console cache:clear --env=prod
